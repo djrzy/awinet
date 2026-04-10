dd5827b000d8ff04aaed2984ad9e39340dbe44e7
djrzy <rzzy_gstandi@yahoo.com>
2026-04-10 13:03:01 +0700

    10 april 2026

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -43,12 +43,13 @@
             </form>
 
         </div>
-        <div class="hidden lg:block lg:w-[45%]">
+        <div class="hidden lg:block lg:w-[60%]">
             {{-- <img src="{{ asset('awinet.png') }}" alt="Company Logo" class="mx-auto h-16 w-auto"> --}}
             <h2 class="text-3xl font-bold text-center text-white mt-4">Welcome to Awinet</h2>
             <p class="text-center text-white mt-2">Please login to your account to continue</p>
         </div>
-        <div class="hidden lg:flex flex-col lg:w-[55%] bg-gray-200 h-screen items-center space-y-3 justify-center">
+        <div
+            class="hidden lg:flex flex-col lg:w-[40%] lg:px- bg-gray-200 h-screen items-center space-y-3 justify-center">
             <img src="{{ asset('awinet.png') }}" alt="Company Logo" class="mx-auto h-16 w-auto">
             <h2 class="text-xl font-semibold text-center">Login</h2>
             <form action="#" method="POST" class="space-y-4 w-full max-w-lg">
@@ -71,7 +72,7 @@
                     </div>
                 </div>
                 <button type="submit"
-                    class="w-full px-4 py-2 font-bold text-white bg-black rounded hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200">
+                    class="w-full px-4 py-2 font-bold text-white bg-black rounded hover:bg-[#007E41] focus:outline-none focus:ring focus:ring-blue-200 cursor-pointer">
                     Login
                 </button>
             </form>
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/login', function () {
+Route::get('/', function () {
     return view('login');
 })->name('login');
 
